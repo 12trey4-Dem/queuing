bc996785c9de22e1f75b9ad2aafdf61f753f1421
12trey4-Dem <demdeclaro@gmail.com>
2026-07-07 19:21:36 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase setup for online player registration.
+// Replace the placeholder values below with your Firebase web app config.
+// The registration page and the main queuing system must use the same config.
+window.SBAHCBC_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB8GEDVvwV-CQ57-pe3T-eNvjJdglTtBZE",
+  authDomain: "queuing-system-1e8c8.firebaseapp.com",
+  databaseURL: "https://queuing-system-1e8c8-default-rtdb.firebaseio.com",
+  projectId: "queuing-system-1e8c8",
+  storageBucket: "queuing-system-1e8c8.firebasestorage.app",
+  messagingSenderId: "943080992098",
+  appId: "1:943080992098:web:49e1d5a250ab7eb6ea503b",
+  measurementId: "G-EXRH419NWT"
+};
